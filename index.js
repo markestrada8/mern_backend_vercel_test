72a3faae3895e2232534f523e6337f1203af7ba5
Mark Estrada <mark.estrada.dev>
2023-03-09 17:49:50 -0600

    route fixing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const express= require('express')
+const express = require('express')
 const dotenv = require('dotenv').config()
 const cors = require('cors')
 const connectDB = require('./config/db')
@@ -9,20 +9,20 @@ const app = express()
 connectDB()
 const PORT = process.env.PORT || 8000
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 
 app.use('/api/todos', require('./routes/todoRoutes'))
 app.use('/api/user', require('./routes/userRoutes'))
 
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')))
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
-} else {
-  app.get('/', (req, res) => res.send('Please set to production'))
-}
+//   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+// } else {
+//   app.get('/', (req, res) => res.send('Please set to production'))
+// }
 
 app.use(errorHandler)
 
