63ed473560e3995f2cd4215d2728e8f755e81058
Mark Estrada <mark.estrada.dev>
2023-03-09 18:05:07 -0600

    route fixing

--- index.js
+++ index.js
@@ -14,16 +14,16 @@ app.use(cors())
 
 
 // if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../frontend/build')))
+//   app.use(express.static(path.join(__dirname, '../build')))
 
-//   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+//   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'build', 'index.html')))
 // } else {
 //   app.get('/', (req, res) => res.send('Please set to production'))
 // }
 
-app.use('/api/todos', require('./routes/todoRoutes'))
-app.use('/api/user', require('./routes/userRoutes'))
-app.get('/', (req, res) => res.send('Please set to production'))
+app.use('/api/todos', require('./api/todoRoutes'))
+app.use('/api/user', require('./api/userRoutes'))
+// app.get('/', (req, res) => res.send('Please set to production'))
 
 
 app.use(errorHandler)
